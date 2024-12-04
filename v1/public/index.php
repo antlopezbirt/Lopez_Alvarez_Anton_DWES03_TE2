aef73211fd3a8a74cfb89473f7578c2cdffd8be0
Antón López <antlopez@birt.eus>
2024-12-04 20:59:19 +0100

    Versión básica funcionando

--- v1/public/index.php
+++ v1/public/index.php
@@ -4,44 +4,56 @@ require '../core/Router.php';
 require '../app/controllers/Items.php';
 
 $url = $_SERVER['QUERY_STRING'];
-echo 'URL = ' . $url . '<br>';
+echo 'QUERY_STRING = ' . $url . '<br>';
+/* echo 'REQUEST_URI = ' . $_SERVER['REQUEST_URI']. '<br>';
+echo 'PHP_SELF = ' . $_SERVER['PHP_SELF']. '<br>';
+echo 'DOCUMENT_ROOT = ' . $_SERVER['DOCUMENT_ROOT']. '<br>';
+echo 'SCRIPT_FILENAME = ' . $_SERVER['SCRIPT_FILENAME']. '<br>';
+echo 'PATH_TRANSLATED = ' . $_SERVER['PATH_TRANSLATED']. '<br>';
+echo 'SCRIPT_NAME = ' . $_SERVER['SCRIPT_NAME']. '<br>';
+echo 'PATH_INFO = ' . $_SERVER['PATH_INFO']. '<br>';
+echo 'ORIG_PATH_INFO = ' . $_SERVER['ORIG_PATH_INFO']. '<br>';
+echo dirname(__FILE__) . "<br>"; */
+
 
 $router = new Router();
 
-$router->add('/v1/public', array(
+$router->add('/', array(
     'controller'=>'Home',
     'action'=>'index'
     )
 );
 
-$router->add('/v1/public/items/create', array(
+$router->add('/items/create', array(
     'controller'=>'Items',
     'action'=>'create'
     )
 );
 
-echo '<pre>';
+/* echo '<pre>';
 print_r($router->getRoutes()) . '<br>';
-echo '</pre>';
+echo '</pre>'; */
 
 $urlParams = explode('/', $url);
 
 $urlArray = array(
     'HTTP'=>$_SERVER['REQUEST_METHOD'],
     'path'=>$url,
-    'controller'=>$urlParams[3],
-    'action'=>$urlParams[4],
-    'params'=>$urlParams[5]
+    'controller'=>$urlParams[1] ?? '',
+    'action'=>$urlParams[2] ?? '',
+    'params'=>$urlParams[3] ?? ''
 );
+
+// var_dump($urlParams);
 
 // Si el controlador está vacío...
 // Si no, recoge los valores de controlador y parámetros
-if (!empty($urlParams[3])) {
-    $urlArray['controller'] = ucwords($urlParams[3]);
-    if (!empty($urlParams[4])) {
-        $urlArray['action'] = $urlParams[4];
-        if(!empty($urlParams[5])) {
-            $urlArray['params'] = $urlParams[5];
+if (!empty($urlParams[1])) {
+    $urlArray['controller'] = ucwords($urlParams[1]);
+    if (!empty($urlParams[2])) {
+        $urlArray['action'] = $urlParams[2];
+        if(!empty($urlParams[3])) {
+            $urlArray['params'] = $urlParams[3];
         }
     } else {
         $urlArray['action'] = 'index';
